b8c50779ca637b9b84338592b90126687d39d3cf
alessandroamormino <amormino.alessandro97@gmail.com>
2023-03-22 13:52:38 +0100

    feat: generated 10 mail with button

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        emails: [],
       }
-    }
+    }, 
+    created(){
+
+    },
+    methods: {
+        getTenRandomEmail(){
+            this.emails = [];
+            for(let i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                    if(res.data.success){
+                        this.emails.push(res.data.response);
+                    }
+                });
+            }
+        }
+    }, 
+
   }).mount('#app')
